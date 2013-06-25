aacc67724a91c7e25c5f27b4af0c91be96d32e8c
Mike Pink <mike.p@dnxmarketing.com>
2013-06-25 16:00:16 +0100

    merged scripts into one

--- createvhost.sh
+++ createvhost.sh
@@ -4,14 +4,22 @@ if [ `whoami` != root ]; then
     exit
 fi
 
+if [ ! "$1" ]; then
+    echo "createhost.sh [add|delete]"
+    exit
+fi
+
 echo "Enter a domain (e.g. dev.example.com):";
 read domain;
 echo ""
-echo "Enter file location (e.g. mydir/myproject):"
-read folder;
-echo ""
 
-if [[ ! -e /etc/apache2/sites-available/${domain}.vhost.conf ]]; then
+if [ "$1" = "add" ]; then
+
+    echo "Enter file location (e.g. mydir/myproject):"
+    read folder;
+    echo ""
+
+    if [[ ! -e /etc/apache2/sites-available/${domain}.vhost.conf ]]; then
 
 echo "
 <VirtualHost *:80>
@@ -27,17 +35,58 @@ echo "
   </Directory>
 </VirtualHost>" >> /etc/apache2/sites-available/${domain}.vhost.conf
 
-    ln -s /etc/apache2/sites-available/${domain}.vhost.conf /etc/apache2/sites-enabled/${domain}.vhost.conf
+        ln -s /etc/apache2/sites-available/${domain}.vhost.conf /etc/apache2/sites-enabled/${domain}.vhost.conf
 
-    echo "${domain} created"
-    echo "Testing configuration"
-    apachectl configtest
-    echo "Would you like me to restart the server? (y/n)"
-    read q
-    if [[ "${q}" == "yes" ]] || [[ "${q}" == "y" ]]; then
-        apachectl restart
+        echo "${domain} created"
+
+        if ! grep "${domain}" /etc/apache2/httpd.conf >> /dev/null; then
+            echo "127.0.0.1 ${domain}" >> /etc/hosts
+            echo "Host file updated"
+        else
+            echo "Already in hosts"
+        fi
+
+        echo "Testing configuration"
+        apachectl configtest
+        echo "Would you like me to restart the server? (y/n)"
+        read q
+        if [[ "${q}" == "yes" ]] || [[ "${q}" == "y" ]]; then
+            apachectl restart
+        fi
+
+    else
+        echo "${domain} already exists"
     fi
 
-else
-    echo "${domain} already exists"
+fi
+
+if [ "$1" = "delete" ]; then
+
+    ### Check to see if $domain exists
+    if [[ -e /etc/apache2/sites-available/${domain}.vhost.conf ]]; then
+
+        echo "Permenantly delete? (y/n)";
+        read delete;
+        echo ""
+
+        rm /etc/apache2/sites-enabled/${domain}.vhost.conf
+        if [[ "${delete}" == "yes" ]] || [[ "${delete}" == "y" ]]; then
+            rm /etc/apache2/sites-available/${domain}.vhost.conf
+
+            if grep "${domain}" /etc/hosts >> /dev/null; then
+                echo "deleting from host file"
+                sed -i.bak "/${domain}/d" /etc/hosts
+            fi
+
+        fi
+        echo "Virtual host deleted"
+        echo "Would you like me to restart the server? (y/n)"
+        read q
+        if [[ "${q}" == "yes" ]] || [[ "${q}" == "y" ]]; then
+                apachectl restart
+        fi
+    else
+        echo "${domain} not found"
+    fi
+
 fi
